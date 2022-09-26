b6eca350601f0bf06c0d2e4cfbee589074353f43
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2022-09-26 16:24:55 -0400

    Setup for Characers
    :heavy_plus_sign: Character class creation

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    /// <summary>
+    /// Characters trust level with player
+    /// </summary>
+    [Range(0, 10)]
+    public int TrustLevel;
+
+
+    public string Name;
+
+    public bool isAlive;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DefaultConvo()
+    {
+
+    }
+}
