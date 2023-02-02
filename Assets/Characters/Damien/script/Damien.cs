3155a1827e64ced182478de382d5e8a333059c9d
Adnan Sage <adnankasujja1@gmail.com>
2023-02-02 15:10:53 -0600

    Experimenting with Hardcoding Dialogue (Damian)
    So....

--- Assets/Characters/Damien/script/Damien.cs
+++ Assets/Characters/Damien/script/Damien.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class Damien : Character
 {
-    
+
+    void Start()
+    {
+        base.Start();
+        /*
+        //Regular
+        DialogRegularConvo ExampleRegularConvo = new DialogRegularConvo();
+        ExampleRegularConvo.Text = "Example";
+        ExampleRegularConvo.NPCTalking = true;
+        dialogForRegularConvo.Add(ExampleRegularConvo);*/
+
+        //Interrogation
+        DialogueForInterrogation ExampleInterrogationConvo = new DialogueForInterrogation();
+        ExampleInterrogationConvo.NextElementNumber = 1;
+        ExampleInterrogationConvo.PlayerTalk = false;
+        ExampleInterrogationConvo.EndInterrogation = false;
+        ExampleInterrogationConvo.NoQuestions = false;
+        ExampleInterrogationConvo.Response = "Example Response";
+        //Creating a Question
+        ExampleInterrogationConvo.Question1.QuestionText = "First Question";
+        ExampleInterrogationConvo.Question1.NextElementNumber = 2;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo);
+    }
 }
+
+/*
+ 
+*/
