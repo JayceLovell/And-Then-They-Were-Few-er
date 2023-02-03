d996d8156bd21f0e3807ae68f9803321742f6306
Adnan Sage <adnankasujja1@gmail.com>
2023-02-02 21:20:20 -0600

    Some changes to the prefabs, and a failed attempt at hardcoding
    As the title sayys

--- Assets/Characters/Old Crazy Man/script/OldCrazyMan.cs
+++ Assets/Characters/Old Crazy Man/script/OldCrazyMan.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OldCrazyMan : Character
-{
-    // Start is called before the first frame update
+{/*
     void Start()
     {
+        base.Start();
         
-    }
+        //Regular
+        //DialogRegularConvo ExampleRegularConvo = new DialogRegularConvo();
+        //ExampleRegularConvo.Text = "Example";
+        //ExampleRegularConvo.NPCTalking = true;
+        //dialogForRegularConvo.Add(ExampleRegularConvo);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        //Interrogation
+        DialogueForInterrogation ExampleInterrogationConvo = new DialogueForInterrogation();
+        ExampleInterrogationConvo.NextElementNumber = 1;
+        ExampleInterrogationConvo.PlayerTalk = false;
+        ExampleInterrogationConvo.EndInterrogation = false;
+        ExampleInterrogationConvo.NoQuestions = false;
+        ExampleInterrogationConvo.Response = "I was in the bathroom.";
+        //Creating a Question
+        ExampleInterrogationConvo.Question1.QuestionText = "Where were you at the time of the murder?";
+        ExampleInterrogationConvo.Question1.NextElementNumber = 1;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo);
+    }*/
 }
+
+/* 
+ Where were you at the time of the murder? element 1
+
+    response: I was in the bathroom.
+
+    Can anyone verify that you were there? element 2
+
+    response: Yes, my son was inside with me.
+     
+     Your son was in the stall with you? Why?? element 3
+
+    response: I always ask him to accompany me so that I have someone to hold my gears. Don’t want to drop them in the toilet!
+*/
