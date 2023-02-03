e7bee9a69daae03e62a2425d744bacfb02a6d20e
Adnan Sage <adnankasujja1@gmail.com>
2023-02-03 00:49:33 -0600

    Daddy Jayce showed me the light 🥺(Old Crazy Man Dialogue Added)
    Making slight progress

--- Assets/Characters/Old Crazy Man/script/OldCrazyMan.cs
+++ Assets/Characters/Old Crazy Man/script/OldCrazyMan.cs
@@ -16,19 +16,101 @@ public class OldCrazyMan : Character
         //dialogForRegularConvo.Add(ExampleRegularConvo);
 
         //Interrogation
+        //Element 0
+
         DialogueForInterrogation ExampleInterrogationConvo = new DialogueForInterrogation();
         ExampleInterrogationConvo.NextElementNumber = 1;
         ExampleInterrogationConvo.PlayerTalk = false;
         ExampleInterrogationConvo.EndInterrogation = false;
         ExampleInterrogationConvo.NoQuestions = false;
-        ExampleInterrogationConvo.Response = "I was in the bathroom.";
-        //Creating a Question
+        ExampleInterrogationConvo.Response = " ";
+        //Creating Questions
 
         ExampleInterrogationConvo.Question1 = new Question();
         ExampleInterrogationConvo.Question1.QuestionText = "Where were you at the time of the murder?";
         ExampleInterrogationConvo.Question1.NextElementNumber = 1;
+
+        ExampleInterrogationConvo.Question2 = new Question();
+        ExampleInterrogationConvo.Question2.QuestionText = " ";
+        ExampleInterrogationConvo.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo.Question3 = new Question();
+        ExampleInterrogationConvo.Question3.QuestionText = " ";
+        ExampleInterrogationConvo.Question3.NextElementNumber = 0;
         //Add unique item
         DialogueForInterrogations.Add(ExampleInterrogationConvo);
+
+        //Element 1
+
+        DialogueForInterrogation ExampleInterrogationConvo1 = new DialogueForInterrogation();
+        ExampleInterrogationConvo1.NextElementNumber = 1;
+        ExampleInterrogationConvo1.PlayerTalk = false;
+        ExampleInterrogationConvo1.EndInterrogation = false;
+        ExampleInterrogationConvo1.NoQuestions = false;
+        ExampleInterrogationConvo1.Response = "I was in the bathroom.";
+        //Creating a Question
+
+        ExampleInterrogationConvo1.Question1 = new Question();
+        ExampleInterrogationConvo1.Question1.QuestionText = "Can anyone verify that you were there?";
+        ExampleInterrogationConvo1.Question1.NextElementNumber = 2;
+
+        ExampleInterrogationConvo1.Question2 = new Question();
+        ExampleInterrogationConvo1.Question2.QuestionText = " ";
+        ExampleInterrogationConvo1.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo1.Question3 = new Question();
+        ExampleInterrogationConvo1.Question3.QuestionText = " ";
+        ExampleInterrogationConvo1.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo1);
+
+        //Element 2
+
+        DialogueForInterrogation ExampleInterrogationConvo2 = new DialogueForInterrogation();
+        ExampleInterrogationConvo2.NextElementNumber = 1;
+        ExampleInterrogationConvo2.PlayerTalk = false;
+        ExampleInterrogationConvo2.EndInterrogation = false;
+        ExampleInterrogationConvo2.NoQuestions = false;
+        ExampleInterrogationConvo2.Response = "Yes, my son was inside with me.";
+        //Creating a Question
+
+        ExampleInterrogationConvo2.Question1 = new Question();
+        ExampleInterrogationConvo2.Question1.QuestionText = "Your son was in the stall with you? Why??";
+        ExampleInterrogationConvo2.Question1.NextElementNumber = 3;
+
+        ExampleInterrogationConvo2.Question2 = new Question();
+        ExampleInterrogationConvo2.Question2.QuestionText = " ";
+        ExampleInterrogationConvo2.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo2.Question3 = new Question();
+        ExampleInterrogationConvo2.Question3.QuestionText = " ";
+        ExampleInterrogationConvo2.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo2);
+
+        //Element 3
+
+        DialogueForInterrogation ExampleInterrogationConvo3 = new DialogueForInterrogation();
+        ExampleInterrogationConvo3.NextElementNumber = 1;
+        ExampleInterrogationConvo3.PlayerTalk = false;
+        ExampleInterrogationConvo3.EndInterrogation = false;
+        ExampleInterrogationConvo3.NoQuestions = false;
+        ExampleInterrogationConvo3.Response = "I always ask him to accompany me so that I have someone to hold my gears. Don’t want to drop them in the toilet!";
+        //Creating a Question
+
+        ExampleInterrogationConvo3.Question1 = new Question();
+        ExampleInterrogationConvo3.Question1.QuestionText = " ";
+        ExampleInterrogationConvo3.Question1.NextElementNumber = 0;
+
+        ExampleInterrogationConvo3.Question2 = new Question();
+        ExampleInterrogationConvo3.Question2.QuestionText = " ";
+        ExampleInterrogationConvo3.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo3.Question3 = new Question();
+        ExampleInterrogationConvo3.Question3.QuestionText = " ";
+        ExampleInterrogationConvo3.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo3);
     }
 }
 
