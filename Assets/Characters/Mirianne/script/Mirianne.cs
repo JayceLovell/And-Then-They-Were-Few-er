8327ae8d771468ebe94d15d7a1e5c6973bdbc0d7
Adnan Sage <adnankasujja1@gmail.com>
2023-02-03 19:45:46 -0600

    Mirianne Interrogation Dialogue Updated
    Hardcode

--- Assets/Characters/Mirianne/script/Mirianne.cs
+++ Assets/Characters/Mirianne/script/Mirianne.cs
@@ -1,18 +1,405 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Mirianne : Character
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        base.Start();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Regular
+        //DialogRegularConvo ExampleRegularConvo = new DialogRegularConvo();
+        //ExampleRegularConvo.Text = "Example";
+        //ExampleRegularConvo.NPCTalking = true;
+        //dialogForRegularConvo.Add(ExampleRegularConvo);
+
+        //Interrogation
+        //Element 0
+
+        DialogueForInterrogation ExampleInterrogationConvo = new DialogueForInterrogation();
+        ExampleInterrogationConvo.NextElementNumber = 1;
+        ExampleInterrogationConvo.PlayerTalk = false;
+        ExampleInterrogationConvo.EndInterrogation = false;
+        ExampleInterrogationConvo.NoQuestions = false;
+        ExampleInterrogationConvo.Response = " ";
+        //Creating Questions
+
+        ExampleInterrogationConvo.Question1 = new Question();
+        ExampleInterrogationConvo.Question1.QuestionText = "Where were you at the time of the murder?";
+        ExampleInterrogationConvo.Question1.NextElementNumber = 1;
+
+        ExampleInterrogationConvo.Question2 = new Question();
+        ExampleInterrogationConvo.Question2.QuestionText = " ";
+        ExampleInterrogationConvo.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo.Question3 = new Question();
+        ExampleInterrogationConvo.Question3.QuestionText = " ";
+        ExampleInterrogationConvo.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo);
+
+        //Element 1
+
+        DialogueForInterrogation ExampleInterrogationConvo1 = new DialogueForInterrogation();
+        ExampleInterrogationConvo1.NextElementNumber = 1;
+        ExampleInterrogationConvo1.PlayerTalk = false;
+        ExampleInterrogationConvo1.EndInterrogation = false;
+        ExampleInterrogationConvo1.NoQuestions = false;
+        ExampleInterrogationConvo1.Response = "I don’t see why this is any of your business, “Detective”";
+        //Creating a Question
+
+        ExampleInterrogationConvo1.Question1 = new Question();
+        ExampleInterrogationConvo1.Question1.QuestionText = "Hey! Don’t tell me you are going to undermine an active murder investigation";
+        ExampleInterrogationConvo1.Question1.NextElementNumber = 2;
+
+        ExampleInterrogationConvo1.Question2 = new Question();
+        ExampleInterrogationConvo1.Question2.QuestionText = " ";
+        ExampleInterrogationConvo1.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo1.Question3 = new Question();
+        ExampleInterrogationConvo1.Question3.QuestionText = " ";
+        ExampleInterrogationConvo1.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo1);
+
+        //Element 2
+
+        DialogueForInterrogation ExampleInterrogationConvo2 = new DialogueForInterrogation();
+        ExampleInterrogationConvo2.NextElementNumber = 1;
+        ExampleInterrogationConvo2.PlayerTalk = false;
+        ExampleInterrogationConvo2.EndInterrogation = false;
+        ExampleInterrogationConvo2.NoQuestions = false;
+        ExampleInterrogationConvo2.Response = "Well, last time I checked, you are not the police." +
+            "I thought you and our dear Mayor were going to fetch real law enforcement once this storm cleared up";
+        //Creating a Question
+
+        ExampleInterrogationConvo2.Question1 = new Question();
+        ExampleInterrogationConvo2.Question1.QuestionText = " ";
+        ExampleInterrogationConvo2.Question1.NextElementNumber = 0;
+
+        ExampleInterrogationConvo2.Question2 = new Question();
+        ExampleInterrogationConvo2.Question2.QuestionText = " ";
+        ExampleInterrogationConvo2.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo2.Question3 = new Question();
+        ExampleInterrogationConvo2.Question3.QuestionText = "I’d just like to know all the facts, before the Mayor and I approach the police.";
+        ExampleInterrogationConvo2.Question3.NextElementNumber = 3;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo2);
+
+        //Element 3
+
+        DialogueForInterrogation ExampleInterrogationConvo3 = new DialogueForInterrogation();
+        ExampleInterrogationConvo3.NextElementNumber = 1;
+        ExampleInterrogationConvo3.PlayerTalk = true;
+        ExampleInterrogationConvo3.EndInterrogation = false;
+        ExampleInterrogationConvo3.NoQuestions = false;
+        ExampleInterrogationConvo3.Response = "(Ashlyn takes a deep breath, trying to control her anger)" +
+            "Ashlyn then sighs, and her tone changes  from interrogative to mournful";
+        //Creating a Question
+
+        ExampleInterrogationConvo3.Question1 = new Question();
+        ExampleInterrogationConvo3.Question1.QuestionText = "There’s a potential murderer on the loose. And Nikki was my best friend. " +
+            "I need to get to the bottom of this";
+        ExampleInterrogationConvo3.Question1.NextElementNumber = 4;
+
+        ExampleInterrogationConvo3.Question2 = new Question();
+        ExampleInterrogationConvo3.Question2.QuestionText = " ";
+        ExampleInterrogationConvo3.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo3.Question3 = new Question();
+        ExampleInterrogationConvo3.Question3.QuestionText = " ";
+        ExampleInterrogationConvo3.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo3);
+
+        //Element 4
+
+        DialogueForInterrogation ExampleInterrogationConvo4 = new DialogueForInterrogation();
+        ExampleInterrogationConvo4.NextElementNumber = 1;
+        ExampleInterrogationConvo4.PlayerTalk = false;
+        ExampleInterrogationConvo4.EndInterrogation = false;
+        ExampleInterrogationConvo4.NoQuestions = false;
+        ExampleInterrogationConvo4.Response = " (Mirianne, who had been glaring at Ashlyn, " +
+            "changes her expression somewhat to a look of resentful understanding)";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo4.Question1 = new Question();
+        ExampleInterrogationConvo4.Question1.QuestionText = "Next";
+        ExampleInterrogationConvo4.Question1.NextElementNumber = 5;
+
+        ExampleInterrogationConvo4.Question2 = new Question();
+        ExampleInterrogationConvo4.Question2.QuestionText = " ";
+        ExampleInterrogationConvo4.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo4.Question3 = new Question();
+        ExampleInterrogationConvo4.Question3.QuestionText = " ";
+        ExampleInterrogationConvo4.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo4);
+
+        //Element 5
+
+        DialogueForInterrogation ExampleInterrogationConvo5 = new DialogueForInterrogation();
+        ExampleInterrogationConvo5.NextElementNumber = 1;
+        ExampleInterrogationConvo5.PlayerTalk = false;
+        ExampleInterrogationConvo5.EndInterrogation = false;
+        ExampleInterrogationConvo5.NoQuestions = false;
+        ExampleInterrogationConvo5.Response = "Well, if there’s one thing that hasn’t changed, it’s how much you’re willing to do for your friends…";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo5.Question1 = new Question();
+        ExampleInterrogationConvo5.Question1.QuestionText = "Next";
+        ExampleInterrogationConvo5.Question1.NextElementNumber = 6;
+
+        ExampleInterrogationConvo5.Question2 = new Question();
+        ExampleInterrogationConvo5.Question2.QuestionText = " ";
+        ExampleInterrogationConvo5.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo5.Question3 = new Question();
+        ExampleInterrogationConvo5.Question3.QuestionText = " ";
+        ExampleInterrogationConvo5.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo5);
+
+        //Element 6
+
+        DialogueForInterrogation ExampleInterrogationConvo6 = new DialogueForInterrogation();
+        ExampleInterrogationConvo6.NextElementNumber = 1;
+        ExampleInterrogationConvo6.PlayerTalk = false;
+        ExampleInterrogationConvo6.EndInterrogation = false;
+        ExampleInterrogationConvo6.NoQuestions = false;
+        ExampleInterrogationConvo6.Response = "If you must know, I was in [some room near the lab].";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo6.Question1 = new Question();
+        ExampleInterrogationConvo6.Question1.QuestionText = "Hold on. The Mayor says he saw you in [different room].";
+        ExampleInterrogationConvo6.Question1.NextElementNumber = 7;
+
+        ExampleInterrogationConvo6.Question2 = new Question();
+        ExampleInterrogationConvo6.Question2.QuestionText = " ";
+        ExampleInterrogationConvo6.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo6.Question3 = new Question();
+        ExampleInterrogationConvo6.Question3.QuestionText = " ";
+        ExampleInterrogationConvo6.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo6);
+
+        //Element 7
+
+        DialogueForInterrogation ExampleInterrogationConvo7 = new DialogueForInterrogation();
+        ExampleInterrogationConvo7.NextElementNumber = 1;
+        ExampleInterrogationConvo7.PlayerTalk = false;
+        ExampleInterrogationConvo7.EndInterrogation = false;
+        ExampleInterrogationConvo7.NoQuestions = false;
+        ExampleInterrogationConvo7.Response = "(Mirianne stutters for a bit, shocked, but quickly recovers)";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo7.Question1 = new Question();
+        ExampleInterrogationConvo7.Question1.QuestionText = "Next";
+        ExampleInterrogationConvo7.Question1.NextElementNumber = 8;
+
+        ExampleInterrogationConvo7.Question2 = new Question();
+        ExampleInterrogationConvo7.Question2.QuestionText = " ";
+        ExampleInterrogationConvo7.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo7.Question3 = new Question();
+        ExampleInterrogationConvo7.Question3.QuestionText = " ";
+        ExampleInterrogationConvo7.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo7);
+
+        //Element 8
+
+        DialogueForInterrogation ExampleInterrogationConvo8 = new DialogueForInterrogation();
+        ExampleInterrogationConvo8.NextElementNumber = 1;
+        ExampleInterrogationConvo8.PlayerTalk = false;
+        ExampleInterrogationConvo8.EndInterrogation = false;
+        ExampleInterrogationConvo8.NoQuestions = false;
+        ExampleInterrogationConvo8.Response = "Oh! Pardon me, I seem to have misremembered. Yes I was indeed in that room.";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo8.Question1 = new Question();
+        ExampleInterrogationConvo8.Question1.QuestionText = "Next";
+        ExampleInterrogationConvo8.Question1.NextElementNumber = 9;
+
+        ExampleInterrogationConvo8.Question2 = new Question();
+        ExampleInterrogationConvo8.Question2.QuestionText = " ";
+        ExampleInterrogationConvo8.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo8.Question3 = new Question();
+        ExampleInterrogationConvo8.Question3.QuestionText = " ";
+        ExampleInterrogationConvo8.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo8);
+
+        //Element 9
+
+        DialogueForInterrogation ExampleInterrogationConvo9 = new DialogueForInterrogation();
+        ExampleInterrogationConvo9.NextElementNumber = 1;
+        ExampleInterrogationConvo9.PlayerTalk = false;
+        ExampleInterrogationConvo9.EndInterrogation = false;
+        ExampleInterrogationConvo9.NoQuestions = false;
+        ExampleInterrogationConvo9.Response = "I could hear two people talking very loudly, but couldn’t make out what they were saying." +
+            "The storm was too loud.";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo9.Question1 = new Question();
+        ExampleInterrogationConvo9.Question1.QuestionText = "What happened next?";
+        ExampleInterrogationConvo9.Question1.NextElementNumber = 10;
+
+        ExampleInterrogationConvo9.Question2 = new Question();
+        ExampleInterrogationConvo9.Question2.QuestionText = " ";
+        ExampleInterrogationConvo9.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo9.Question3 = new Question();
+        ExampleInterrogationConvo9.Question3.QuestionText = " ";
+        ExampleInterrogationConvo9.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo9);
+
+        //Element 10
+
+        DialogueForInterrogation ExampleInterrogationConvo10 = new DialogueForInterrogation();
+        ExampleInterrogationConvo10.NextElementNumber = 1;
+        ExampleInterrogationConvo10.PlayerTalk = false;
+        ExampleInterrogationConvo10.EndInterrogation = false;
+        ExampleInterrogationConvo10.NoQuestions = false;
+        ExampleInterrogationConvo10.Response = "At the time, I heard a very loud sound, which I assumed was the thunder." +
+            "But now that I think about it…";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo10.Question1 = new Question();
+        ExampleInterrogationConvo10.Question1.QuestionText = "What?";
+        ExampleInterrogationConvo10.Question1.NextElementNumber = 11;
+
+        ExampleInterrogationConvo10.Question2 = new Question();
+        ExampleInterrogationConvo10.Question2.QuestionText = " ";
+        ExampleInterrogationConvo10.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo10.Question3 = new Question();
+        ExampleInterrogationConvo10.Question3.QuestionText = " ";
+        ExampleInterrogationConvo10.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo10);
+
+        //Element 11
+
+        DialogueForInterrogation ExampleInterrogationConvo11 = new DialogueForInterrogation();
+        ExampleInterrogationConvo11.NextElementNumber = 1;
+        ExampleInterrogationConvo11.PlayerTalk = false;
+        ExampleInterrogationConvo11.EndInterrogation = false;
+        ExampleInterrogationConvo11.NoQuestions = false;
+        ExampleInterrogationConvo11.Response = "(Miriane begins to scowl)";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo11.Question1 = new Question();
+        ExampleInterrogationConvo11.Question1.QuestionText = "Next?";
+        ExampleInterrogationConvo11.Question1.NextElementNumber = 12;
+
+        ExampleInterrogationConvo11.Question2 = new Question();
+        ExampleInterrogationConvo11.Question2.QuestionText = " ";
+        ExampleInterrogationConvo11.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo11.Question3 = new Question();
+        ExampleInterrogationConvo11.Question3.QuestionText = " ";
+        ExampleInterrogationConvo11.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo11);
+
+        //Element 12
+
+        DialogueForInterrogation ExampleInterrogationConvo12 = new DialogueForInterrogation();
+        ExampleInterrogationConvo12.NextElementNumber = 1;
+        ExampleInterrogationConvo12.PlayerTalk = false;
+        ExampleInterrogationConvo12.EndInterrogation = false;
+        ExampleInterrogationConvo12.NoQuestions = false;
+        ExampleInterrogationConvo12.Response = "…It may have been a body hitting the ground.";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo12.Question1 = new Question();
+        ExampleInterrogationConvo12.Question1.QuestionText = "Oh...";
+        ExampleInterrogationConvo12.Question1.NextElementNumber = 13;
+
+        ExampleInterrogationConvo12.Question2 = new Question();
+        ExampleInterrogationConvo12.Question2.QuestionText = " ";
+        ExampleInterrogationConvo12.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo12.Question3 = new Question();
+        ExampleInterrogationConvo12.Question3.QuestionText = " ";
+        ExampleInterrogationConvo12.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo12);
+
+        //Element 13
+
+        DialogueForInterrogation ExampleInterrogationConvo13 = new DialogueForInterrogation();
+        ExampleInterrogationConvo13.NextElementNumber = 1;
+        ExampleInterrogationConvo13.PlayerTalk = false;
+        ExampleInterrogationConvo13.EndInterrogation = false;
+        ExampleInterrogationConvo13.NoQuestions = false;
+        ExampleInterrogationConvo13.Response = "(Miriane’s expression hardens)";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo13.Question1 = new Question();
+        ExampleInterrogationConvo13.Question1.QuestionText = "Next";
+        ExampleInterrogationConvo13.Question1.NextElementNumber = 14;
+
+        ExampleInterrogationConvo13.Question2 = new Question();
+        ExampleInterrogationConvo13.Question2.QuestionText = " ";
+        ExampleInterrogationConvo13.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo13.Question3 = new Question();
+        ExampleInterrogationConvo13.Question3.QuestionText = " ";
+        ExampleInterrogationConvo13.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo13);
+
+        //Element 14
+
+        DialogueForInterrogation ExampleInterrogationConvo14 = new DialogueForInterrogation();
+        ExampleInterrogationConvo14.NextElementNumber = 1;
+        ExampleInterrogationConvo14.PlayerTalk = false;
+        ExampleInterrogationConvo14.EndInterrogation = false;
+        ExampleInterrogationConvo14.NoQuestions = false;
+        ExampleInterrogationConvo14.Response = "I didn’t pay it any mind. Shortly after, I left for my room." +            "May I go now, “Detective”?";
+
+        //Creating a Question
+
+        ExampleInterrogationConvo14.Question1 = new Question();
+        ExampleInterrogationConvo14.Question1.QuestionText = "Yes. And Mirianne, Thank You";
+        ExampleInterrogationConvo14.Question1.NextElementNumber = 0;
+
+        ExampleInterrogationConvo14.Question2 = new Question();
+        ExampleInterrogationConvo14.Question2.QuestionText = " ";
+        ExampleInterrogationConvo14.Question2.NextElementNumber = 0;
+
+        ExampleInterrogationConvo14.Question3 = new Question();
+        ExampleInterrogationConvo14.Question3.QuestionText = " ";
+        ExampleInterrogationConvo14.Question3.NextElementNumber = 0;
+        //Add unique item
+        DialogueForInterrogations.Add(ExampleInterrogationConvo14);
+
+
     }
 }
+
+/* 
+ Potential Dialogue  
+ 
+
+
+*/
