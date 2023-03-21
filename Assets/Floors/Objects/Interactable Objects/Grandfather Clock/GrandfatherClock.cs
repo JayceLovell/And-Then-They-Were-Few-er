264f6d19c26c433ce09b9d83a8776dd3f21e89a1
Adnan Sage <adnankasujja1@gmail.com>
2023-03-21 00:06:05 -0500

    Small tweaks to GdaddyCLock, Endings added for Fred, Jays, Mirr and Rach

--- Assets/Floors/Objects/Interactable Objects/Grandfather Clock/GrandfatherClock.cs
+++ Assets/Floors/Objects/Interactable Objects/Grandfather Clock/GrandfatherClock.cs
@@ -18,28 +18,28 @@ public class GrandfatherClock : Objects
         DialogueForObjects Dialogue1 = new DialogueForObjects();
         Dialogue1.JustAshlynTalking = true;
         Dialogue1.EndInteraction = false;
-        Dialogue1.Text = "Hmmm this clock clearly isn't working.";
+        Dialogue1.Text = "Hmmm, this clock clearly isn't working.";
 
         DialogueForObject.Add(Dialogue1);
 
         DialogueForObjects Dialogue2 = new DialogueForObjects();
         Dialogue2.JustAshlynTalking = true;
         Dialogue2.EndInteraction = false;
-        Dialogue2.Text = "Even if it did the design.";
+        Dialogue2.Text = "Even though it looks functional, something's... off";
 
         DialogueForObject.Add(Dialogue2);
 
         DialogueForObjects Dialogue3 = new DialogueForObjects();
         Dialogue3.JustAshlynTalking = true;
         Dialogue3.EndInteraction = false;
-        Dialogue3.Text = "It doesn't have enough notches";
+        Dialogue3.Text = "It just doesn't have enough notches.";
 
         DialogueForObject.Add(Dialogue3);
 
         DialogueForObjects Dialogue4 = new DialogueForObjects();
         Dialogue4.JustAshlynTalking = true;
         Dialogue4.EndInteraction = true;
-        Dialogue4.Text = "Who made this?";
+        Dialogue4.Text = "Who built this? It definitely wasn't Nikki...";
 
         DialogueForObject.Add(Dialogue4);
     }
